a0aff0ccdf07d8edd0970469540f70e66c77cdf8
rusbev <ozboy94@gmail.com>
2015-11-10 08:59:10 -0500

    v1.3

--- p1/SaloonGunfight.py
+++ p1/SaloonGunfight.py
@@ -1,5 +1,6 @@
 __author__ = 'Jake Mackay'
 # A program for simulating the dealing of 13 cards to 4 people from a deck.
+# Updated for 3.x compatibility
 
 
 import random
@@ -7,12 +8,10 @@ import random
 SUITS = ["Spades", "Hearts", "Diamonds", "Clubs"]
 RANKS = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
          "Jack", "Queen", "King"]
-
 PLAYERS = ["North", "East", "South", "West"]
 
 CARDS = 52
 CARDS_PER_HAND = 13
-
 
 def main():
     # Create a deck as a list of integers from 0 to 51
@@ -20,22 +19,19 @@ def main():
 
     # Shuffle the deck
     random.shuffle(deck)
-    dealt = 0
 
-    # Loops to deal until deck is empty
+    # Loops to deal until deck is empty, counts hands dealt
     for i in range(1, 14):
-        print "Dealing hand #", dealt + 1
-        dealt = dealt + 1
+        print("Dealing hand #", i)
         # print(deck)
 
         # Loops to deal to table
         for j in range(0, 4):
             card = deck.pop(0)
 
-            print PLAYERS[j], "got the", RANKS[card % CARDS_PER_HAND], "of", SUITS[card // CARDS_PER_HAND]
-        print"\n"
+            print(PLAYERS[j], "got the", RANKS[card % CARDS_PER_HAND], "of", SUITS[card // CARDS_PER_HAND])
+        print("\n")
 
-    print"Whiskey was spilt, game was interrupted by necessary shootout"
-
+    print("Whiskey was spilt, game was interrupted by necessary shootout")
 
 main()
